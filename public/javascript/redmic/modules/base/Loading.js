7ddf4777a137cb5feeed47eb47566423cf9b6625
Pedro Trujillo <pedro@redmic.es>
2019-10-01 14:16:44 +0100

    Limpia también cargando global en caso de error
    Al realizar la búsqueda desde el nodo donde se coloca la carga global,
    esta no se estaba encontrando (solo hijos suyos). Para incluirle en la
    búsqueda de nodos con carga pendiente, se busca en todo el documento.

--- public/javascript/redmic/modules/base/Loading.js
+++ public/javascript/redmic/modules/base/Loading.js
@@ -251,7 +251,7 @@ define([
 				return;
 			}
 
-			var loadingParentNodes = query('[loading]', this.globalNode);
+			var loadingParentNodes = query('[loading]');
 
 			for (var i = 0; i < loadingParentNodes.length; i++) {
 				var loadingParentNode = loadingParentNodes[i];

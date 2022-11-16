2e9af0a99b6a2d63245fde33a813efaa46c746ff
Pedro Trujillo <pedroetb@gmail.com>
2022-11-16 10:03:50 +0000

    Elimina opciones de node para compilación

--- grunt/task/buildApp.js
+++ grunt/task/buildApp.js
@@ -23,12 +23,11 @@ module.exports = function(grunt) {
 				var profile = pkg.dojoBuild,
 					scriptPath = path.join(srcPath, 'dojo/dojo.js'),
 					releaseDir = path.join(rootPath, distPath),
-					nodeParams = ' --optimize_for_size --max_old_space_size=3000 --gc_interval=100 ',
 					buildParams = ' load=build --profile "' + profile + '" --releaseDir "' + releaseDir + '"';
 
 				return [
 					'echo "\nBuilding application with ' + profile + ' to ' + releaseDir + '\n"',
-					'node' + nodeParams + scriptPath + buildParams,
+					'node ' + scriptPath + buildParams,
 					'echo "\nBuild complete"'
 				].join('; ');
 			}

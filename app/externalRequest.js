2d715f1f81e68363afcef74f7690b37ee1df0fc1
Pedro Trujillo <pedroetb@gmail.com>
2023-11-20 12:15:31 +0000

    Corrige sintaxis

--- app/externalRequest.js
+++ app/externalRequest.js
@@ -194,7 +194,7 @@ function onSitemapRequest(_req, res) {
 			logger.error('Missing sitemap URL, set it using SITEMAP_URL environment variable');
 
 			const emptySitemap = '<?xml version="1.0" encoding="UTF-8"?>\n<urlset ' +
-				'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml"/>'
+				'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml"/>';
 
 			res.send(emptySitemap);
 			return;

f1cfb8729295e344408902a39bdd6d8e5ed9de83
Pedro Trujillo <pedroetb@gmail.com>
2025-02-21 14:53:10 +0000

    Corrige inclusión de parámetro de GTM para dev

--- client-app/src/util/CookieLoader.js
+++ client-app/src/util/CookieLoader.js
@@ -107,7 +107,7 @@ define([
 					(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-					'https://www.googletagmanager.com/gtm.js?id='+i+dl+${gtmDevParams};f.parentNode.insertBefore(j,f);
+					'https://www.googletagmanager.com/gtm.js?id='+i+dl+'${gtmDevParams}';f.parentNode.insertBefore(j,f);
 					})(window,document,'script','dataLayer','${gtmId}');
 				`,
 				headScriptElement = globalThis.document.createElement('script'),
